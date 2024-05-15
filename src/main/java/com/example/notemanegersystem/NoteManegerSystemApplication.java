49c27460ae68bf82282c037e3ba59269c9be48c6
PHAN DUC <ducpa2002@gmail.com>
2024-05-15 22:53:30 +0700

    first commit

---
+++ src/main/java/com/example/notemanegersystem/NoteManegerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.notemanegersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteManegerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteManegerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notemanegersystem/NoteManegerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notemanegersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteManegerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
